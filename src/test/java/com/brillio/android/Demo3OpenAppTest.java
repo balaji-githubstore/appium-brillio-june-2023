e4db844a40eaebb265ad58ff1a26beccfa449175
balaji-githubstore <balaji0017@gmail.com>
2023-06-08 11:46:54 +0530

    logic for checking presence of element

--- src/test/java/com/brillio/android/Demo3OpenAppTest.java
+++ src/test/java/com/brillio/android/Demo3OpenAppTest.java
@@ -13,25 +13,37 @@ public class Demo3OpenAppTest {
 
 	public static void main(String[] args) throws MalformedURLException, InterruptedException {
 
-		DesiredCapabilities cap=new DesiredCapabilities();
+		DesiredCapabilities cap = new DesiredCapabilities();
 		cap.setCapability("platformName", "android");
 		cap.setCapability("appium:deviceName", "mydevice-redmi");
 		cap.setCapability("appPackage", "org.khanacademy.android");
 		cap.setCapability("appActivity", "org.khanacademy.android.ui.library.MainActivity");
 		cap.setCapability("noReset", true);
-		
-		AppiumDriver driver=new AppiumDriver(new URL("http://localhost:4723/wd/hub"),cap);
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
-		
-		driver.findElement(AppiumBy.xpath("//android.widget.TextView[@text='Dismiss']")).click();
-		//click on settings
-		// click on sign in 
-		//click on sign up with email 
-		//fill firstname, lastname, dob - Dec 20, 2000,  password, create 
-		
-		
+
+		AppiumDriver driver = new AppiumDriver(new URL("http://localhost:4723/wd/hub"), cap);
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+
+		// check visibility - it will not work here because Dismiss is not present
+//		if(driver.findElement(AppiumBy.xpath("//android.widget.TextView[@text='Dismiss']")).isDisplayed())
+//		{
+//			driver.findElement(AppiumBy.xpath("//android.widget.TextView[@text='Dismiss']")).click();
+//		}
+
+		//check presence of element
+		if (driver.findElements(AppiumBy.xpath("//android.widget.TextView[@text='Dismiss']")).size() > 0) 
+		{
+			driver.findElement(AppiumBy.xpath("//android.widget.TextView[@text='Dismiss']")).click();
+		}
+
+		driver.findElement(AppiumBy.xpath("//android.widget.TextView[@text='Sign in']")).click();
+
+		driver.findElement(AppiumBy.xpath("//android.widget.TextView[@text='Sign in']")).click();
+
+		driver.findElement(AppiumBy.xpath("//android.widget.EditText[contains(@text,'address or')]"))
+				.sendKeys("john@gmail.com");
+
 		Thread.sleep(5000);
-		
+
 		driver.quit();
 	}
 
