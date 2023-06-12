deb6f25a7e3a86e0ee871ddc0d84fb635a931c08
balaji-githubstore <balaji0017@gmail.com>
2023-06-12 11:46:01 +0530

    Tap code

--- src/test/java/com/brillio/android/Demo12TapTest.java
+++ src/test/java/com/brillio/android/Demo12TapTest.java
@@ -13,6 +13,7 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 
 import io.appium.java_client.AppiumBy;
 import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
 
 public class Demo12TapTest {
 
@@ -25,7 +26,7 @@ public class Demo12TapTest {
 		cap.setCapability("appActivity", "org.khanacademy.android.ui.library.MainActivity");
 		cap.setCapability("noReset", true);
 
-		AppiumDriver driver = new AppiumDriver(new URL("http://localhost:4723/wd/hub"), cap);
+		AndroidDriver driver = new AndroidDriver(new URL("http://localhost:4723/wd/hub"), cap);
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
 
 		// check presence of element
@@ -44,9 +45,8 @@ public class Demo12TapTest {
 		Actions actions = new Actions(driver);
 
 		PointerInput finger = new PointerInput(Kind.TOUCH, "finger");
-		
+		//tap 10 times on the xpath //*[@text='OFF']
 		for (int i = 1; i <= 10; i++) {
-
 			actions.tick(finger.createPointerMove(Duration.ofMillis(0), PointerInput.Origin.viewport(), point.x + 10,
 					point.y + 10)).tick(finger.createPointerDown(PointerInput.MouseButton.MIDDLE.asArg()))
 					.tick(new Pause(finger, Duration.ofMillis(600)))
@@ -56,6 +56,7 @@ public class Demo12TapTest {
 		Thread.sleep(5000);
 
 		driver.quit();
+		//will start at 11:15 AM IST
 	}
 
 }
